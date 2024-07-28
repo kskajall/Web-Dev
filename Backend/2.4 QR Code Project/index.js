101a4e0101b4c278055bd337e950fa017855c517
kskajall <gaurikajalsharma2002@gmail.com>
2024-07-28 12:06:33 +0530

    QR Code project

--- Backend/2.4 QR Code Project/index.js
+++ Backend/2.4 QR Code Project/index.js
@@ -3,6 +3,8 @@
 */
 
 import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
 
 inquirer
   .prompt([{
@@ -13,7 +15,14 @@ inquirer
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
-    console.log(answers);
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr-image.png'));
+
+    fs.writeFile("URL.txt", url, (err) => {
+        if (err) throw err;
+        console.log("The file has been saved!");
+    });
   })
   .catch((error) => {
     if (error.isTtyError) {
