fcf2942f7ef34afa76c1ebdb9cd48c215045ca36
kskajall <gaurikajalsharma2002@gmail.com>
2024-07-29 19:04:19 +0530

    HTTP Requests

---
+++ Backend/3.2 HTTP Requests/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>hello</h1>");
+});
+
+app.listen(port, () => {
+    console.log(`Serve is listening on post ${port}.`)
+});
